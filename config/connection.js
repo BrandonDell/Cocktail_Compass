314969d98068f8776fd046e5b4055f2d11fb5697
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-13 12:54:51 -0400

    initial folder structure

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres'
+  }
+);
+
+module.exports = sequelize;

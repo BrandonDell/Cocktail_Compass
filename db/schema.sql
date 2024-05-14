15a3f233ea1ee7c376f3e6a4a98fbe1fd9b9c5c4
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-14 18:33:26 -0400

    Added integration for user

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS cocktail_compass_db;
 CREATE DATABASE cocktail_compass_db;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);

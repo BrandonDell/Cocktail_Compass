20f3e6a6c5339e302fc619e37dc735fdf06b2b08
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-15 12:38:04 -0400

    Added comments

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+// In models/Comment.js
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    recipeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipe',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;

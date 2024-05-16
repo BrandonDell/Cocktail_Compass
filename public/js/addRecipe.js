fcf633f511c6d521130b6c44e014777c0dab8f80
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-15 21:50:44 -0400

    Made changes to add recipe to replace homepage

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,21 +1,21 @@
 const newFormHandler = async function (event) {
-    event.preventDefault();
-  
-    const recipe_name = document.querySelector('input[name="recipe_name"]').value;
-    const recipe_content = document.querySelector('textarea[name="recipe_content"]').value;
-  
-    await fetch(`/api/recipe`, {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        postBody,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  
-    document.location.replace("/dashboard");
-  };
-  
-  document
-    .querySelector("#new-post-form")
-    .addEventListener("submit", newFormHandler);
+  event.preventDefault();
+
+  const recipe_name = document.querySelector('input[name="recipe_name"]').value;
+  const recipe_content = document.querySelector('textarea[name="recipe_content"]').value;
+
+  await fetch(`/api/recipe`, {
+    method: "POST",
+    body: JSON.stringify({
+      recipe_name,
+      recipe_content,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  document.location.replace("/homepage");
+};
+
+document
+  .querySelector("#new-post-form")
+  .addEventListener("submit", newFormHandler);

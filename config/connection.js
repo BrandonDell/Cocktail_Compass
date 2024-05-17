33512fd4776106e2b2c4d52153059a8d01de9c0a
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-05-17 11:13:24 -0400

    render connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres'
-  }
-);
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
+}
 
 module.exports = sequelize;
